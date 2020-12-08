93b1610900659f74492542b0ae60054b997123fe
Peter Bellström <peter.bellstrom@kau.se>
2020-12-08 15:36:08 +0100

    Uppdaterat W7

--- W7/slutkod/kodexempelW7.php
+++ W7/slutkod/kodexempelW7.php
@@ -2,7 +2,7 @@
 <html lang="sv">
     <head>
         <meta charset="utf-8">
-        <title>PHP F6</title>
+        <title>PHP W7 - Databaskopplingar</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
             integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -18,7 +18,7 @@
 
     <body class="container p-2">
         <header class="jumbotron text-center">
-            <h1>PHP F1</h1>
+            <h1>PHP - W7 Databaskopplingar</h1>
         </header>
 
         <main>    
@@ -35,7 +35,6 @@
                     6. Matcha namngiven behållare med värde.
                     7. Exekvera frågan.
                     8. Stäng ner databaskopplingen.
-
 
                 */
 
@@ -63,6 +62,42 @@
                         echo("<p>Summan blev: $summa</p>");
                         echo("<p>Antalet gånger du kastat de 6 tärningarna är: $antal");
 
+                        //Observera att lösningen innehåller mycket redundant kod som vi behöver ta hand om på nästa föreläsning!
+                        //1.
+                        if($summa >= 100) {
+                            
+                            //3.
+                            $dns = "mysql:host=localhost;dbname=dicegame;charset=utf8";
+                            $userName = "root"; //Skapa gärna en egen användare med lösenord!
+                            $password = "";
+            
+                            $dbh = new PDO($dns, $userName, $password);
+
+                            //4.
+                            $sql = "INSERT INTO highscore(points, rounds, nickname) VALUES(:points, :rounds, :nickname);";
+                            
+                            //5.
+                            $stmt = $dbh->prepare($sql);
+
+                            //6.
+                            $stmt->bindValue(":points", $summa);
+                            $stmt->bindValue(":rounds", $antal);
+                            $stmt->bindValue(":nickname", $nickname);
+
+                            //7.
+                            $stmt->execute();
+
+                            //8.
+                            $dbh = null;
+                            $stmt = null; //Vid insert, update och delete är inte detta något krav men vid select behövs det!
+
+                            //2.
+                            $summa = 0;
+                            $antal = 0;
+
+                            echo("<p>För nytt spel tryck på 'Skicka'!</p>");
+                        }
+                        
                     } else {
                         echo("<p>Ange nickname!</p>");
                     }
@@ -70,7 +105,7 @@
 
 
             ?>
-            <form action="kodexempelW7.php" method="post">
+            <form action="<?php echo($_SERVER["PHP_SELF"]); ?>" method="post">
                 <input type="text" name="nickname" value="<?php echo($nickname); ?>" />
                 <input type="submit" name="skicka" value="Skicka" />
                 <input type="submit" name="rensa" value="Rensa" />
@@ -82,6 +117,34 @@
 
                 //Lista innehållet i tabellen highscore som li-element i ett ol-element.
                 //När ni gör utsökningen sortera resultatet i fallande ordning efter rounds.
+
+                $dns = "mysql:host=localhost;dbname=dicegame;charset=utf8";
+                $userName = "root"; //Skapa gärna en egen användare med lösenord!
+                $password = "";
+
+                $dbh = new PDO($dns, $userName, $password);
+
+                $sql = "SELECT id, nickname, points, rounds, date FROM highscore ORDER BY rounds asc, points desc, date asc LIMIT 10;";
+                
+                $stmt = $dbh->prepare($sql);
+
+                $stmt->execute();
+
+                echo("<ol>" . PHP_EOL);
+                while( $row = $stmt->fetch() ) {
+
+                    //$id = $row["id"];
+                    $nickname = $row["nickname"];
+                    $rounds = $row["rounds"];
+                    $points = $row["points"];
+                    $date = $row["date"];
+
+                    echo("<li>$nickname $rounds $points $date</li>");
+                }
+                echo("</ol>" . PHP_EOL);
+
+                $dbh = null;
+                $stmt = null; 
 
             ?>
         </main>
