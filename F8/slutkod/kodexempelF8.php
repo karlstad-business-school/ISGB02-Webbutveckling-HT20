b23f2532b2669978f71048d2d62a387b17e4da72
Peter Bellström <peter.bellstrom@kau.se>
2020-12-14 12:09:15 +0100

    Uppdaterat Slutkod F8

--- F8/slutkod/kodexempelF8.php
+++ F8/slutkod/kodexempelF8.php
@@ -6,14 +6,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
             integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <style> 
-            img {
-                width: 15%;
-                height: 15%;
-                padding-right: 5px;
-                padding-bottom: 10px;
-            }
-        </style>
     </head>
 
     <body class="container p-2">
@@ -35,37 +27,105 @@
                   5. Anropa getHTMLTable() och skrivut returdata från metoden.
                 */
 
-                $dns = "mysql:host=localhost;dbname=demodice;charset=utf8";
-                $userName = "root";
-                $password = "";
+                //När ni är klara placera alla funktioner i en extern PHP-fil och inkludera den med
+                //include();
 
-                $dbh = new PDO($dns, $userName, $password);
+                //Funktionerna
+                function createTable($table) {
 
-                //echo("Fungerar!");
+                    $htmlTable = "<table class='table table-border'>" . PHP_EOL;
 
-                $sql = "SELECT * FROM nbrofdices;";
+                    //funktionsanrop...
+                    $htmlTable .= createTableHead($table[0]);
+                    $htmlTable .= createTableBody($table);
 
-                $stmt = $dbh->prepare($sql);
+                    $htmlTable .= "</table>" . PHP_EOL;
 
-                $stmt->execute();
+                    return $htmlTable;
 
-                //Visa data som vi fick i retur från databasen
-                while( $row = $stmt->fetch() ) {
-
-                    $id = $row["id"];
-                    $one = $row["one"];
-                    $six = $row["six"];
-                    $timestamp = $row["timestamp"];
-
-                    echo("<p>id: $id, one: $one, six: $six, timestamp: $timestamp</p>");
 
                 }
 
-                //echo("Fungerar!");
+                function createTableHead($oneRow) {
+
+                    $htmlHead = "<thead><tr>" . PHP_EOL;
+
+                    foreach($oneRow as $key => $value) {
+                        $htmlHead .= "<th>$key</th>";
+                    }
+
+                    $htmlHead .= "</tr></thead>" . PHP_EOL;
+
+                    return $htmlHead;
+                }
+
+                function createTableBody($table) {
+
+                    $htmlBody = "<tbody>";
+
+                    foreach($table as $row) {
+                        $htmlBody .= createTableRow($row);
+                    }
+
+                    $htmlBody .= "</tbody>";
+
+                    return $htmlBody;
+
+                }
+
+                function createTableRow($oneRow) {
+                    //Fortsätter efter lunch på Workshopen!
+
+                }
+
+                function createTableColumns($oneRow) {
+                    //Fortsätter efter lunch på Workshopen!
+                }
+
+
+                try {
+                    $dns = "mysql:host=localhost;dbname=demodice;charset=utf8";
+                    $userName = "root"; //Skapa gärna en egen användare med lösenord och viss rättighet.
+                    $password = "";
+                    $dbhOptions = array( PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION );
+
+                    $dbh = new PDO($dns, $userName, $password, $dbhOptions);
+
+                    //echo("Fungerar!");
+
+                    $sql = "SELECT * FROM nbrofdices;";
+
+                    $stmt = $dbh->prepare($sql);
+
+                    $stmt->execute();
+
+                    //Visa data som vi fick i retur från databasen
+                    /*
+                    while( $row = $stmt->fetch() ) {
+
+                        $id = $row["id"];
+                        $one = $row["one"];
+                        $six = $row["six"];
+                        $timestamp = $row["timestamp"];
+
+                        echo("<p>id: $id, one: $one, six: $six, timestamp: $timestamp</p>");
+
+                    }
+                    */
+                    //echo("Fungerar!");
+
+                    $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                    $htmlTable = createTable($data);
+                    echo($htmlTable);
+
+                    
+                } catch(PDOException $e) { //Men Exception fungerar också!
+                    echo("<p>" . $e->getMessage() . "</p>");
+                }
 
                 $stmt = null;
                 $dbh = null;
-                  
+
             ?>
 
         </main>
